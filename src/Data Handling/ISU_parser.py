52c349908c2329bac755fa11b1075ea11c5d7c42
L33t <maxim.y.mischenko@gmail.com>
2024-02-15 20:49:19 +0300

    [feat]: done persons publications and rids parsing methods

--- src/Data Handling/ISU_parser.py
+++ src/Data Handling/ISU_parser.py
@@ -1,55 +1,93 @@
+import json
+from random import random
+
 import requests
 from time import sleep
-from random import random
 from bs4 import BeautifulSoup as BSoup
 
 
 class ParserISU:
-    def __init__(self, cookie: str, remember_sso, ids_isu: list = None):
-        if ids_isu is None:
-            ids_isu = []
-        self.ids_isu = ids_isu
-        self.base_isu_link = 'https://isu.itmo.ru/person/'
+    def __init__(self, cookie: str, remember_sso):
         self.cookies = {'ISU_AP_COOKIE': cookie,
                         'REMEMBER_SSO': remember_sso}
 
-    def get_users_data(self):
-        # files = [('file', open('Cookies', 'rb').read()),
-        #          ('file', open('Trust Tokens', 'rb').read())]
+    def parse_users_data(self):
+        base_isu_person_link = 'https://isu.itmo.ru/person/'
 
-        for isu_user_id in range(103466, int(1e6)):
-            sleep(0.02)
-            user_link = self.base_isu_link + str(isu_user_id)
+        for isu_person_id in range(103466, int(1e6)):
+            sleep(0.2 + random())
+            person_link = base_isu_person_link + str(isu_person_id)
             try:
-                response = requests.get(user_link, cookies=self.cookies, timeout=3)
-                print(isu_user_id, response.url, response.status_code)
-                # print(response.text)
-                soup = BSoup(response.text, 'html.parser')
-                print(soup.findAll('span', id='PERSON_PUBLICATION'))
+                response = requests.get(person_link, cookies=self.cookies)
+                print(isu_person_id, response.status_code)
+                person_data = self.parse_data_from_html(response.text)
             except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
-                print(f'User with ISU id {isu_user_id} does not exist')
+                print(f'User with ISU id {isu_person_id} does not exist')
 
+            open('page.html', 'w', encoding='utf-8').write(response.text)
             break
 
-    def test(self):
-        session = requests.Session()
-        files = [('file', open('Cookies', 'rb').read()),
-                 ('file', open('Trust Tokens', 'rb').read())]
+    def parse_data_from_html(self, html_text):
+        soup = BSoup(html_text, 'html.parser')
 
-        response = session.post(self.base_isu_link, cookies=self.cookies, files=files)
+        data = {'publications': self.parse_publications(soup), 'rid': self.parse_rid(soup)}
+        # data = {'rid': self.parse_rid(soup)}
 
-        if response.status_code == 200:
-            data_url = 'https://isu.ifmo.ru/'
-            data_response = session.get(data_url, files=files, cookies=self.cookies)
-            print(data_response.text)
-            print(data_response.url)
-        else:
-            print(response)
-            print('pizza not found')
+        return data
+
+    def parse_publications(self, soup: BSoup):
+        data: str = str(soup.find('span', id='R1724073431179133097').find('script'))
+
+        publications = []
+        data = (data[data.find('.jsonData={') + 10:data.find('};') + 1])
+        data: dict = json.loads(data)
+        data.pop('recordsFiltered')
+
+        for row in data['data']:
+            year_index = row[3].find('>') + 1
+            publications.append({'type': row[1],
+                                 'year': int(row[3][year_index:year_index + 4]),
+                                 'authors': self.parse_authors(row[2]),
+                                 'title': row[2][row[2].rfind('</a>') + 5:]
+                                 })
+        return publications
+
+    def parse_rid(self, soup: BSoup):
+        data: str = str(soup.find('span', id='R1724086259370226350').find('script'))
+        data: dict = json.loads(data[data.find('jsonData={') + 9:data.find('};') + 1])
+        data.pop('recordsFiltered')
+
+        rids = []
+        for row in data['data']:
+            year_index = row[1].find('>') + 1
+            rids.append({
+                'year': int(row[1][year_index:year_index + 4]),
+                'type': row[2].strip(),
+                'title': row[3].strip(),
+                'authors': self.parse_authors(row[5])
+            })
+        return rids
+
+    @staticmethod
+    def parse_authors(authors_string: str) -> list:
+        authors = []
+        for author in authors_string.split(','):
+            first_quote = author.find('"')
+            second_quote = first_quote + author[first_quote + 1:].find('"') + 1
+            third_quote = second_quote + author[second_quote + 1:].find('"') + 1
+            fourth_quote = third_quote + author[third_quote + 1:].find('"') + 1
+            author = {'isu_profile': author[first_quote + 1:second_quote],
+                      'name': author[third_quote + 1: fourth_quote]}
+            authors.append(author)
+        return authors
 
 
 if __name__ == '__main__':
-    parser = ParserISU('yours cookie', 'yours_remember_sso_token')
+    parser = ParserISU('your_cookie',
+                       'your_sso_token')
 
-    parser.get_users_data()
-    # parser.test()
+    # parser.parse_users_data()
+    print(parser.parse_data_from_html(open('page.html', 'r', encoding='utf-8').read()))
+
+# ["<center><span class=\"npr text-primary-click \"
+#

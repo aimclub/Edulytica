4f14c5faad6e733031796759c645612d44f528d3
L33t <maxim.y.mischenko@gmail.com>
2024-05-27 23:39:41 +0300

    [tests]: added tests

--- tests/data_handling/test_pdf_parser.py
+++ tests/data_handling/test_pdf_parser.py
@@ -1,28 +1,31 @@
-import os.path
 import unittest
-from src.data_handling.pdf_parser import PDFParser
+import os
 
 
 class PDFParserTest(unittest.TestCase):
     def setUp(self):
-        self.tests_folder = './test/data_handling/'
-        self.test_persons_file = f'{self.tests_folder}test.pdf'
+        from src.data_handling.pdf_parser import PDFParser
+        self.tests_folder = os.path.abspath('./tests/data_handling/') + '/'
+        self.test_dir = 'test_dir/'
+        self.test_persons_file = f'{self.tests_folder}{self.test_dir}test.pdf'
         self.pdf_parser = PDFParser()
 
     def test_parse_file(self):
         with self.assertRaises(FileNotFoundError):
-            PDFParser().parse_file(f'{self.tests_folder}filenotexists.pdf')
+            self.pdf_parser.parse_file(f'{self.tests_folder}{self.test_dir}filenotexists.pdf')
         self.assertIsInstance(self.pdf_parser.parse_file(self.test_persons_file), str)
 
     def test_parser_files(self):
-        with self.assertRaises(UnboundLocalError):
-            PDFParser().parse_files(f'{self.tests_folder}directorynotexists')
+        with self.assertRaises(FileNotFoundError):
+            self.pdf_parser.parse_files(f'{self.tests_folder}directorynotexists')
         self.assertFalse(os.path.exists(f'{self.tests_folder}testresult.csv'))
-        self.assertIsNone(self.pdf_parser.parse_files(f'{self.tests_folder}', f'{self.tests_folder}testresult.csv'))
+        self.assertIsNone(self.pdf_parser.parse_files(f'{self.tests_folder}', 'testresult.csv'))
+        print(os.listdir(f'{self.tests_folder}'))
         self.assertTrue(os.path.exists(f'{self.tests_folder}testresult.csv'))
 
     def tearDown(self):
         if os.path.exists(f'{self.tests_folder}testresult.csv'):
+            print('deleted')
             os.remove(f'{self.tests_folder}testresult.csv')
 
 

1bbba35724c480cf1fa2905fbaea3d58986c494d
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-02 01:11:57 +0300

    [feat]: Done docker-compose, added migration script

--- src/edulytica_api/docker-entrypoint.sh
+++ src/edulytica_api/docker-entrypoint.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Ожидаем, пока база данных будет готова к подключению..."
-until pg_isready -h edulytica_db -p 5432; do
+set -e
+
+export POSTGRES_USER="edulytica"
+export POSTGRES_PASSWORD="edulyticapassword"
+export POSTGRES_DB="edulytica"
+export PGPASSWORD="edulyticapassword"
+
+until psql -h "edulytica_db" -U "edulytica" -d "edulytica" -c '\q'; do
+  >&2 echo "Waiting Postgres..."
   sleep 2
 done
 
-echo "Запуск миграций Alembic..."
+echo "PostgreSQL is available. Starting migrations..."
+
 alembic upgrade head
 
-echo "Запуск API..."
-exec "$@"
+echo "Migrations are done."
+
+exec uvicorn src.edulytica_api.app:app --host 0.0.0.0 --port 8000

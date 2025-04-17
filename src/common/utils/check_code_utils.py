2552e670786c46ac46beb696276f3d75b50ce5d5
MrAmfix <nat.rud.ufa@gmail.com>
2025-04-17 23:09:09 +0300

    [doc]: Added documentations in check_code and email utils

--- src/common/utils/check_code_utils.py
+++ src/common/utils/check_code_utils.py
@@ -1,12 +1,17 @@
-from datetime import datetime, timedelta
+"""
+This module provides utility functions for generating one-time verification codes.
+
+Functions:
+    generate_code: Generates a 6-digit numeric verification code.
+"""
 from random import randint
-from src.common.utils.moscow_datetime import datetime_now_moscow, set_moscow_timezone
 
 
 def generate_code():
+    """
+    Generates a 6-digit numeric verification code as a string.
+
+    Returns:
+        str: A 6-digit numeric string representing the verification code.
+    """
     return str(randint(1000000, 9999999))[1:]
-
-
-def check_expired_code(created_date: datetime):
-    return (set_moscow_timezone(created_date) < datetime_now_moscow()
-            - timedelta(seconds=60))

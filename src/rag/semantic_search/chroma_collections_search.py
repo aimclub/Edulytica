aaf32a99157d871ee096f73e9a3062f2ce32ea3a
ybondv <yuriybondarev99@gmail.com>
2025-04-18 10:07:06 +0300

    Changes to ChromaDB module

--- src/rag/semantic_search/chroma_collections_search.py
+++ src/rag/semantic_search/chroma_collections_search.py
@@ -4,31 +4,45 @@ import json
 from copy import deepcopy
 
 class ChromaSearcher:
+    """
+    A class to create ChromaDB collections from given specifics and to search in these collections.
+    """
 
-    # CHROMA_HOST = "localhost"  # change
-    # CHROMA_PORT = 8000
     COLLECTIONS_FILE = "all_collections.json"
-    OPENAI_EMBEDDING_MODEL = "text-embedding-3-large"
-    OPENAI_API_KEY = "API_KEY"
+    EMBEDDING_MODEL = "BAAI/bge-m3"
 
     def __init__(self, host, port):
+        """
+        Initialization of ChromaDB client. Requires host and port.
+        """
         self.host = host
         self.port = port
+        self.embedding_function = chromadb.utils.embedding_functions.SentenceTransformerEmbeddingFunction(model_name=ChromaSearcher.EMBEDDING_MODEL)
         self.chroma_client = chromadb.HttpClient(host=self.host, port=self.port)
-        # self.chroma_client = chromadb.Client()
-        self.openai_ef = chromadb.utils.embedding_functions.OpenAIEmbeddingFunction(api_key=ChromaSearcher.OPENAI_API_KEY, model_name=ChromaSearcher.OPENAI_EMBEDDING_MODEL)
+        t = []
+        with open(ChromaSearcher.COLLECTIONS_FILE, "w", encoding='utf-8') as f:
+            json.dump(t, f, ensure_ascii=False)
+            f.close()
 
     def find_similar_records(self, collection_name, texts: list, n_results=5):
-        # Поиск похожих записей в коллекции. Необходимо предоставить название коллекции collection_name и список искомых текстов texts
-        # Дополнительно можно задать количество найденных записей. По умолчанию 5
-        collection = self.chroma_client.get_collection(collection_name, embedding_function=self.openai_ef)
+        """
+        A method for searching parts of specifics that could be similar to a given text
+        Arguments:
+        - collection_name: str - A name of a collection where parts should be searched
+        - texts: list - A lists of texts (str) to be found (compared)
+        - n_results: int - Number of search results to return (by default 5)
+        """
+        collection = self.chroma_client.get_collection(collection_name, embedding_function=self.embedding_function)
         return collection.query(query_texts=texts, n_results=n_results, include=["documents", "metadatas"])
 
     def add_specific(self, file_name, sheet_name, collection_name):
-        # Функция для добавления специфики из файла. Аргументы:
-        # file_name - имя файла, в котором сохранена специфика (str).
-        # sheet_name - название листа, в котором специфика сохранена (str).
-        # collection_name - имя коллекции, в которой сохранить специфику конференции (str).
+        """
+        A method for adding specific to ChromaDB collection
+        Arguments:
+        - file_name: str - A spreadsheet file with specific data.
+        - sheet_name: str - Name of a sheet from spreadsheet file.
+        - collection_name: str - Name of ChromaDB collection to be created. Must be Latin letters and numbers only.
+        """
         spec_data = pd.read_excel(file_name, sheet_name=sheet_name, header=[0,1,2,3])
         spec_data = spec_data.fillna("")
         res = {}
@@ -54,12 +68,13 @@ class ChromaSearcher:
             metadatas.append(deepcopy(md))
         with open(ChromaSearcher.COLLECTIONS_FILE, "r", encoding='utf-8') as f:
             all_collections = json.load(f)
+            f.close()
         all_collections.append({})
         all_collections[-1]['conf_title'] = spec_data.columns[0][0]
         all_collections[-1]['conf_title_short'] = sheet_name
         all_collections[-1]['collection_name'] = collection_name
         
-        collection = self.chroma_client.create_collection(collection_name, embedding_function=self.openai_ef)
+        collection = self.chroma_client.create_collection(collection_name, embedding_function=self.embedding_function)
         collection.add(
             metadatas=metadatas,
             documents=documents,
@@ -67,19 +82,14 @@ class ChromaSearcher:
         )
         with open(ChromaSearcher.COLLECTIONS_FILE, "w", encoding='utf-8') as f:
             json.dump(all_collections, f, ensure_ascii=False)
+            f.close()
 
 
     def get_specifics(self, collection_name):
-        collection = self.chroma_client.get_collection(collection_name, embedding_function=self.openai_ef)
+        """
+        A method to get collection contents by name
+        Arguments:
+        - collection_name: str - A name of ChromaDB collection (must be an existing collection)
+        """
+        collection = self.chroma_client.get_collection(collection_name, embedding_function=self.embedding_function)
         return collection.get(include=["documents", "embeddings", "metadatas"])
-
-DATA_FILE = ""  # path to Excel file with table
-
-if __name__ == "__main__":
-    cs = ChromaSearcher('localhost', 8000)
-    cs.add_specific("Специфика редактированная.xlsx","ППС","PPS")
-    cl = cs.get_specifics("PPS")
-    cqq = cs.find_similar_records("PPS", ["Статья должна быть оформлена"])
-    print(cl)
-    print(cqq)
-    cs.chroma_client.delete_collection("PPS")

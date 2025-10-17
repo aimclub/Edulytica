401d21d61037989100226c491671c391d11a1fbb
MrAmfix <nat.rud.ufa@gmail.com>
2025-10-17 18:55:07 +0300

    [feat]: Fixed Rag tests, examples moves to tools/emaples/

--- tests/rag/test_prompt_enricher.py
+++ tests/rag/test_prompt_enricher.py
@@ -1,8 +1,6 @@
 import unittest
-import os
 from unittest.mock import patch, MagicMock
-
-from Edulytica.src.rag.core.prompt_enricher.prompt_enricher import PromptEnricher
+from src.rag.core.prompt_enricher.prompt_enricher import PromptEnricher
 
 
 class TestPromptEnricher(unittest.TestCase):
@@ -12,7 +10,7 @@ class TestPromptEnricher(unittest.TestCase):
         """Set up the test environment"""
         # Create a mock for ConfigLoader
         self.config_loader_patcher = patch(
-            'Edulytica.src.rag.core.prompt_enricher.prompt_enricher.ConfigLoader')
+            'src.rag.core.prompt_enricher.prompt_enricher.ConfigLoader')
         self.mock_config_loader = self.config_loader_patcher.start()
 
         # Configure mock to return a known prefix value
